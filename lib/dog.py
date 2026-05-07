b0b61cf79a0287be55a181dc17bdc2dc73aa72c3
Shurem-pro <husseinkadir1227@gmail.com>
2026-05-07 15:37:32 +0300

    final

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name} is born!")
+
+    def bark(self):
+        print(f"{self.name} says Woof!")
+
+fido = Dog("Fido")
+# Fido is born!
+
+snoopy = Dog("Snoopy")
+# Snoopy is born!
